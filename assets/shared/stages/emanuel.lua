04170003eb6ec5d22824b070709d7be3b3b2be24
GuimodsSKR <165672877+Guimod@users.noreply.github.com>
2024-09-21 11:18:46 -0300

    Initial commit

---
+++ assets/shared/stages/emanuel.lua
@@ -0,0 +1,12 @@
+--Create with TieGuo's Lua Stage Editor
+function onCreate()
+makeLuaSprite('ciu', 'ciu', -690, -160)
+addLuaSprite('ciu', false)
+scaleObject('ciu', 1.3, 1.3)
+setScrollFactor('ciu', 1, 1)
+
+makeLuaSprite('chao', 'chao', -660, -170)
+addLuaSprite('chao', false)
+scaleObject('chao', 1.3, 1.3)
+setScrollFactor('chao', 1, 1)
+end
